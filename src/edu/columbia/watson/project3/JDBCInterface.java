b9fc612eb0b519c6b6345a76ce962aed8eab6179
bajajabhi1 <bajajabhi1@gmail.com>
2014-05-11 22:30:22 -0400

    jdfc interface added

--- src/edu/columbia/watson/project3/JDBCInterface.java
+++ src/edu/columbia/watson/project3/JDBCInterface.java
@@ -1,65 +1,104 @@
 package edu.columbia.watson.project3;
 
-import java.net.URL;
-
-import java.awt.List;
-import java.io.*;
-import java.net.MalformedURLException;
-import java.util.AbstractList;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.io.PrintWriter;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class JDBCInterface {
-	Connection con = null;
-    Statement st = null;
-    ResultSet rs = null;
-    ResultSet rs1 = null;
-    String url = "jdbc:mysql://localhost:3306/twitter_trigram";
-    String user = "root";
-    String password = "9999813800appu";
-	public void connectDatabase(){
+	static Connection con = null;
+	static Statement st = null;
+	static ResultSet rs = null;
+	static ResultSet rs1 = null;
+	static String url = "jdbc:mysql://localhost:3306/twitter_trigram";
+	static String user = "root";
+	static String password = "9999813800appu";
+	private static String DB_END_POINT = "160.39.178.13";
+	private final static String DB_USER_NAME = "root";
+	private final static String DB_PWD = "9999813800appu";
+	private final static String DB_NAME = "twitter_trigram";
+	private final static int DB_PORT = 3306;
+
+	public static void createConnectionAndStatement() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException
+	{
+		//try
+		//{
+			// This will load the MySQL driver, each DB has its own driver
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			// Setup the connection with the DB
+			String connUrl = "jdbc:mysql://"+DB_END_POINT+":"+DB_PORT+"/"+DB_NAME ;
+			System.out.println(connUrl);
+			con = DriverManager.getConnection(connUrl,DB_USER_NAME,DB_PWD);
+
+			// Statements allow to issue SQL queries to the database
+			st = con.createStatement();
+		/*} catch (Exception e) {
+			e.printStackTrace();
+			close();
+		}*/
+	}
+
+	// You need to close the resultSet
+	private static void close() {
 		try {
-            con = DriverManager.getConnection(url, user, password);
-            
-            st = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
-                    ResultSet.CONCUR_UPDATABLE);
-            
-            String searchQuery  ;
-            searchQuery= "SELECT * from LMI_1000_l200 where word1 = \"british\"";
-            st.executeUpdate(searchQuery);
-            
+			if (rs != null) {
+				rs.close();
+			}
 
-        } catch (SQLException ex) {
-            Logger lgr = Logger.getLogger(Version.class.getName());
-            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+			if (st != null) {
+				st.close();
+			}
 
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-                if (st != null) {
-                    st.close();
-                }
-                if (con != null) {
-                    con.close();
-                }
+			if (con != null) {
+				con.close();
+			}
+		} catch (Exception e) {
 
-            } catch (SQLException ex) {
-                Logger lgr = Logger.getLogger(Version.class.getName());
-                lgr.log(Level.WARNING, ex.getMessage(), ex);
-            }
-        }
-		
-		
+		}
+	}
+
+	public static void connectDatabase(){
+		try 
+		{
+			createConnectionAndStatement();
+
+			String searchQuery = "SELECT * from LMI_1000_l200 where word1 = \"british\"";
+			st.executeQuery(searchQuery);
+
+
+		} catch (SQLException ex) {
+			System.out.println("Exception - " + ex.getMessage());
+
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (st != null) {
+					st.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+
+			} catch (SQLException ex) {
+				System.out.println("Exception - " + ex.getMessage());
+			}
+		}		
+	}
+
+	public static void main(String args[])
+	{
+		connectDatabase();
 	}
 }
